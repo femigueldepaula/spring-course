b0a9bd9d0336a6408ffcd3ed3452a34bc63296dc
Felipe de Paula <femigueldepaula@gmail.com>
2021-03-29 19:01:38 -0300

    alterando .properties para .yml e criação do docker compose para rodar local

--- src/main/java/com/spring/course/SpringCourseApplication.java
+++ src/main/java/com/spring/course/SpringCourseApplication.java
@@ -1,17 +1,16 @@
 package com.spring.course;
 
-import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.spring")
 public class SpringCourseApplication extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(Application.class);
+		return builder.sources(SpringCourseApplication.class);
 	}
 
 	public static void main(String[] args) {
