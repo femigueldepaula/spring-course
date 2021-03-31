c2debde75a99b68aca6f83a9830498a8b3bc844f
Felipe de Paula <femigueldepaula@gmail.com>
2021-03-30 21:13:11 -0300

    Configuração do authenticationManger e ignore o enpoint de criação do token da validação do spring security.

--- src/main/java/com/spring/course/security/SecurityConfig.java
+++ src/main/java/com/spring/course/security/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.spring.course.security;
 
-import com.spring.course.service.UserService;
 import com.spring.course.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -12,14 +16,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private UserServiceImpl userService;
+    @Autowired private UserServiceImpl userService;
 
-    @Autowired
-    private CustomerPasswordEncoder passwordEncoder;
+    @Autowired private CustomerPasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
+    }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.POST, "/users/login");
     }
 }
